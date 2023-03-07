a0dec771aac3c64da53ca8eaf32eeca05ab5afa7
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-03-07 13:41:33 +0100

    repro for roslyn bug

---
+++ AnalyzersNotTriggeredRepo/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AnalyzersNotTriggeredRepo
+{
+    public class Class1
+    {
+        public class NonSonarWay
+        {
+            public static void EnabledByRuleset(int a)
+            {
+                if (a == 1)
+                {
+                    Console.WriteLine("Cheking...");
+                }
+                else if (a == 2)
+                {
+                    Console.WriteLine("Something else");
+                }
+                else if (a == 3) // S126 should be raised here when enabled by ruleset or .editorconfig
+                {
+                    Console.WriteLine("S126 should be raised here....");
+                }
+            }
+
+            public static async void S3168() { } // Raise S3168
+        }
+    }
+}
